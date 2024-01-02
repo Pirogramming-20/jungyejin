b18bb27854abc725e3476f150805da15a47d22cd
serenityjung <wjd7542@cau.ac.kr>
2024-01-03 03:26:58 +0900

    5. JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,12 +1,27 @@
+//JSON파일에서 아이템 fetch
 function loadItems() {
   return fetch("data/data.json")
     .then((Response) => Response.json())
     .then((json) => json.items);
 }
+// 패치된 아이템 리스트 업데이트
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
 
+// HTML에 리스트 추가할 것 만들기
+function createHTMLString(item) {
+  return `
+      <li class="item">
+          <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+          <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+      `;
+}
 //메인
 loadItems().then((items) => {
   console.log(items);
-  //   displayItems(items);
+  displayItems(items);
   //   setEventListeners(items);
 });
