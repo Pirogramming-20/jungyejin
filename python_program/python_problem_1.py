f50b4a74465a7a90dbb0dbf56f24bd56760b3431
serenityjung <wjd7542@cau.ac.kr>
2024-01-05 03:59:19 +0900

    prob1_6

--- python_program/python_problem_1.py
+++ python_program/python_problem_1.py
@@ -1,30 +1,36 @@
+import sys
 
 num=0
-
 while True:
-    try:
-        next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if 0 < next_num and next_num < 4:
-            break
-        else:
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
+    while True:
+        try:
+            next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if 0 < next_num and next_num < 4:
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
 
-for i in range(next_num):
-    num=num+1
-    print("playerA : ",num)
+    for i in range(next_num):
+        num=num+1
+        print("playerA : ",num)
+        if num==31 :
+            sys.exit()
 
-while True:
-    try:
-        next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if 0 < next_num and next_num < 4:
-            break
-        else:
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
+    while True:
+        try:
+            next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if 0 < next_num and next_num < 4:
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
 
-for i in range(next_num):
-    num=num+1
-    print("playerB : ",num)
+    for i in range(next_num):
+        num=num+1
+        print("playerB : ",num)
+        if num==31 :
+            sys.exit()
+        
