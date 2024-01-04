134536e632f744b49fbee900d94ba3b08243c605
serenityjung <wjd7542@cau.ac.kr>
2024-01-05 05:13:58 +0900

    prob1_9

--- python_program/python_problem_1.py
+++ python_program/python_problem_1.py
@@ -1,34 +1,37 @@
 import sys
+import random
 
-num=0
 
 def br_game(user, num):
-    while True:
-        try:
-            next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if 0 < next_num and next_num < 4:
-                break
-            else:
-                print("1,2,3 중 하나를 입력하세요")
-        except ValueError:
-            print("정수를 입력하세요")
+    if user==True:
+        next_num=random.randint(1, 3)
+    else:
+        while True:
+            try:
+                next_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+                if 0 < next_num and next_num < 4:
+                    break
+                else:
+                    print("1,2,3 중 하나를 입력하세요")
+            except ValueError:
+                print("정수를 입력하세요")
 
     for i in range(next_num):
         num=num+1
         if(user==True):
-            print("playerA : ",num)
+            print("computer : ",num)
         else:
-            print("playerB : ",num)
+            print("player : ",num)
         if num==31 :
             if(user==True):
-                print("playerB win!")
+                print("player win!")
             else:
-                print("playerA win!")
+                print("computer win!")
             sys.exit()
     return num
 
 
-user=True
+user=True           #true면 컴퓨터, false면 플레이어
 num=0
 while True:
     num=br_game(user,num)
